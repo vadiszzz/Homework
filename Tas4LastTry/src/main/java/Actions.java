42b64ab86a473bf1220dcbad7fe01a628a56d377
vadiszzz <vadim.spb.korobey@gmail.com>
2020-11-22 18:39:05 +0300

    Переделал структуру немного и добавил тестов

--- Tas4LastTry/src/main/java/Actions.java
+++ Tas4LastTry/src/main/java/Actions.java
@@ -3,6 +3,7 @@ import com.yandex.disk.rest.ProgressListener;
 import com.yandex.disk.rest.RestClient;
 import com.yandex.disk.rest.exceptions.ServerException;
 import com.yandex.disk.rest.exceptions.ServerIOException;
+import com.yandex.disk.rest.exceptions.http.UnauthorizedException;
 import com.yandex.disk.rest.json.DiskInfo;
 import com.yandex.disk.rest.json.Link;
 
@@ -10,65 +11,84 @@ import java.io.File;
 import java.io.IOException;
 
 public class Actions {
-    Credentials credentials;
+    public static RestClient restClient;
+    public static boolean authorized;
+    //  public boolean authorized;
 
-    public Actions(Credentials credentials) {
-        if (credentials==null){
-            throw new NullPointerException("Credentials can not be null");
+    public Actions(String user, String token) {
+        if ((user == null) || (token == null)) {
+            throw new NullPointerException("User or token can not be null");
         }
-        this.credentials = credentials;
+        Credentials credentials = new Credentials(user, token);
+        restClient = new RestClient(credentials);
+        authorized = true;
+        try {
+            restClient.getDiskInfo();
+        } catch (IOException e) {
+        } catch (ServerIOException e) {
+            System.out.println("Invalid token, not authorized");
+            authorized = false;
+        }
     }
 
-    public Link MakeFolder(String path) {
-        if (path==null){
+    public Actions(RestClient restClient) throws IOException, ServerIOException {
+        if (restClient == null) {
+            throw new NullPointerException("RestClient can not be null");
+        }
+        this.restClient = restClient;
+        authorized = true;
+    }
+
+    static public Link makeFolder(String path) {
+        if (path == null) {
             throw new NullPointerException("Path can not be null");
         }
-        RestClient restClient = new RestClient(credentials);
         Link result = null;
         try {
             result = restClient.makeFolder(path);
         } catch (IOException e) {
         } catch (ServerIOException e) {
+            authorized = false;
         }
         return result;
     }
 
-    public void DownloadFile(final String path, final File saveTo, final ProgressListener progressListener) {
+    public void downloadFile(final String path, final File saveTo, final ProgressListener progressListener) {
         if (path == null) {
             throw new NullPointerException("Path can not be null");
         }
         if (saveTo == null) {
             throw new NullPointerException("Save directory can not be null");
         }
-        RestClient restClient = new RestClient(credentials);
         try {
             restClient.downloadFile(path, saveTo, progressListener);
         } catch (IOException e) {
         } catch (ServerException e) {
+            authorized = false;
         }
     }
 
-    public DiskInfo DiskInformation() {
-        RestClient restClient = new RestClient(credentials);
+    public DiskInfo diskInformation() {
         DiskInfo result = null;
         try {
             result = restClient.getDiskInfo();
         } catch (IOException e) {
         } catch (ServerIOException e) {
+            authorized = false;
         }
         return result;
     }
 
-    public Link DeleteFile(String path, boolean permanently) {
+    public Link deleteFile(String path, boolean permanently) {
         if (path == null) {
             throw new NullPointerException("Path can not be null");
         }
-        RestClient restClient = new RestClient(credentials);
         Link result = null;
         try {
             result = restClient.delete(path, permanently);
         } catch (IOException e) {
         } catch (ServerIOException e) {
+            authorized = false;
         }
         return result;
     }
--- Tas4LastTry/src/test/java/ActionsTest.java
+++ Tas4LastTry/src/test/java/ActionsTest.java
@@ -1,71 +1,138 @@
 
-import com.yandex.disk.rest.Credentials;
+
 import com.yandex.disk.rest.ProgressListener;
 import com.yandex.disk.rest.RestClient;
 import com.yandex.disk.rest.exceptions.ServerException;
 import com.yandex.disk.rest.exceptions.ServerIOException;
-import com.yandex.disk.rest.json.DiskInfo;
-import com.yandex.disk.rest.json.Link;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
 
 import java.io.File;
 import java.io.IOException;
 
 public class ActionsTest {
     @Test
-    public void testMakeFolderCorrect() {
+    public void InvalidToken() {
+        String user = "vadiszzz";
+        String token = "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud5";
+        Actions actions = new Actions(user, token);
+        Assertions.assertFalse(actions.authorized);
+    }
+
+    @Test
+    public void CorrectToken() {
+        String user = "vadiszzz";
+        String token = "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4";
+        Actions actions = new Actions(user, token);
+        Assertions.assertTrue(actions.authorized);
+    }
+
+    @Test
+    public void testMakeFolderMock() throws IOException, ServerIOException {
         String path = "NewFolder1";
-        Credentials credentials = new Credentials("vadiszzz", "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4");
-        Actions yandexDisk = new Actions(credentials);
-        Assertions.assertDoesNotThrow(() -> yandexDisk.MakeFolder(path));
+        RestClient restClient = Mockito.mock(RestClient.class);
+        Actions actions = new Actions(restClient);
+        actions.makeFolder(path);
+        Mockito.verify(restClient, Mockito.times(1)).makeFolder(path);
     }
+
     @Test
-    public void testMakeFolderNull(){
+    public void testMakeDownloadFileMock() throws IOException, ServerException {
+        String path = "path";
+        RestClient restClient = Mockito.mock(RestClient.class);
+        Actions actions = new Actions(restClient);
+        File saveTo = new File("C://someFolder");
+        ProgressListener progressListener = null;
+        actions.downloadFile(path, saveTo, progressListener);
+        Mockito.verify(restClient, Mockito.times(1)).downloadFile(path, saveTo, progressListener);
+    }
+
+    @Test
+    public void testDiskInfoMock() throws IOException, ServerIOException {
+        RestClient restClient = Mockito.mock(RestClient.class);
+        Actions actions = new Actions(restClient);
+        actions.diskInformation();
+        Mockito.verify(restClient, Mockito.times(1)).getDiskInfo();
+    }
+
+    @Test
+    public void testDeleteMock() throws IOException, ServerIOException {
+        String path = "path";
+        RestClient restClient = Mockito.mock(RestClient.class);
+        Actions actions = new Actions(restClient);
+        actions.deleteFile(path, true);
+        Mockito.verify(restClient, Mockito.times(1)).delete(path, true);
+    }
+
+    @Test
+    public void testMakeFolderCorrect() throws IOException, ServerIOException {
+        String path = "NewFolder1";
+        String user = "vadiszzz";
+        String token = "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4";
+        Actions yandexDisk = new Actions(user, token);
+        System.out.println(yandexDisk.diskInformation());
+        Assertions.assertDoesNotThrow(() -> yandexDisk.makeFolder(path));
+    }
+
+    @Test
+    public void testMakeFolderNull() throws IOException, ServerIOException {
         String path = null;
-        Credentials credentials = new Credentials("vadiszzz", "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4");
-        Actions yandexDisk = new Actions(credentials);
-        Assertions.assertThrows(NullPointerException.class,() ->yandexDisk.MakeFolder(path));
+        String user = "vadiszzz";
+        String token = "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4";
+        Actions yandexDisk = new Actions(user, token);
+        Assertions.assertThrows(NullPointerException.class, () -> yandexDisk.makeFolder(path));
     }
+
     @Test
-    public void testCredentialsNull(){
-        Credentials credentials = null;
-        Assertions.assertThrows(NullPointerException.class,()-> new Actions(credentials));
+    public void testCredentialsNull() {
+        String user = null;
+        String token = "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4";
+        Assertions.assertThrows(NullPointerException.class, () -> new Actions(user, token));
     }
+
     @Test
-    public void testDownloadFilePathIsNull(){
+    public void testDownloadFilePathIsNull() throws IOException, ServerIOException {
         String path = null;
-        Credentials credentials = new Credentials("vadiszzz", "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4");
-        Actions yandexDisk = new Actions(credentials);
+        String user = "vadiszzz";
+        String token = "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4";
+        Actions yandexDisk = new Actions(user, token);
         ProgressListener progressListener = null;
         File saveTo = new File("C://someFolder");
-        Assertions.assertThrows(NullPointerException.class,() ->  yandexDisk.DownloadFile(path,saveTo,progressListener));
+        Assertions.assertThrows(NullPointerException.class, () -> yandexDisk.downloadFile(path, saveTo, progressListener));
     }
+
     @Test
-    public void testDownloadFileSaveToIsNull(){
+    public void testDownloadFileSaveToIsNull() throws IOException, ServerIOException {
         String path = "path";
-        Credentials credentials = new Credentials("vadiszzz", "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4");
-        Actions yandexDisk = new Actions(credentials);
+        String user = "vadiszzz";
+        String token = "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4";
+        Actions yandexDisk = new Actions(user, token);
         ProgressListener progressListener = null;
         File saveTo = null;
-        Assertions.assertThrows(NullPointerException.class,() ->  yandexDisk.DownloadFile(path,saveTo,progressListener));
+        Assertions.assertThrows(NullPointerException.class, () -> yandexDisk.downloadFile(path, saveTo, progressListener));
     }
+
     @Test
-    public void testDownloadNormal(){
+    public void testDownloadNormal() throws IOException, ServerException {
         String path = "path";
-        Credentials credentials = new Credentials("vadiszzz", "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4");
-        Actions yandexDisk = new Actions(credentials);
+        String user = "vadiszzz";
+        String token = "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4";
+        Actions yandexDisk = new Actions(user, token);
         ProgressListener progressListener = null;
         File saveTo = new File("C://someFolder");
-        Assertions.assertDoesNotThrow(() ->  yandexDisk.DownloadFile(path,saveTo,progressListener));
+        Assertions.assertDoesNotThrow(() -> yandexDisk.downloadFile(path, saveTo, progressListener));
     }
+
     @Test
-    public void testDeleteNullPath(){
+    public void testDeleteNullPath() throws IOException, ServerIOException {
         String path = null;
-        Credentials credentials = new Credentials("vadiszzz", "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4");
-        Actions yandexDisk = new Actions(credentials);
-        Assertions.assertThrows(NullPointerException.class,() ->yandexDisk.DeleteFile(path,true));
+        String user = "vadiszzz";
+        String token = "AgAAAAAyz5SlAADLW0WIDd2ZYkI1oMPA8eq3ud4";
+        Actions yandexDisk = new Actions(user, token);
+        Assertions.assertThrows(NullPointerException.class, () -> yandexDisk.deleteFile(path, true));
     }
 
 
